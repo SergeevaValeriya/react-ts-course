d8f8f26046e0e6052fdc2544c3cf99be176f4e04
Valeriya <vvsergeeva@inbox.ru>
2021-10-28 22:47:21 +0300

    Creating card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import Card, {CardVariant} from "./components/Card";
 
 const App = () => {
     return (
         <div>
-          Working!
+          <Card
+              onClick={() => console.log('click')}
+              variant={CardVariant.outlined}
+              width="200px" height="200px">
+              <button>Button</button>
+              <div>Text</div>
+          </Card>
         </div>
     );
 };
